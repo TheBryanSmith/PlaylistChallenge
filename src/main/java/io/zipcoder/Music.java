a65bedccf2b32ec231e9f3ee2e0b27a1b264d9de
Bryan <bryan@Zipcoders-MacBook-Pro-10.local>
2024-08-01 20:35:26 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -7,8 +7,23 @@ public class Music {
     public Music(String[] playList){
         this.playList = playList;
     }
+        int forward,backwards = 0;
+    public Integer selection(Integer startIndex, String selection) {
+      for(int i = startIndex; i < playList.length; i++) {
+          if (playList[i].equals(selection)) {
+              break;
+          } else {
+              forward++;
+          }
+      }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+          for (int i = startIndex; i >= 0; i--){
+              if(playList[i].equals(selection)){
+                  break;
+              } else {
+                  backwards++;
+              }
+          }
+       return Math.min(backwards,forward);
     }
 }
